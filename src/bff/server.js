a25eeee0a577eb78bf59af581bcffbacf2280c72
Dmitry <dsilichev@gmail.com>
2024-07-12 14:41:13 +0300

    Реализовать получение и проверку ролей

--- src/bff/server.js
+++ src/bff/server.js
@@ -1,58 +1,7 @@
-import { addUser } from './add-user';
-import { getUser } from './get-user';
-import { sessions } from './sessions';
+import { authorize, logout, register} from './operations';
 
 export const server = {
-  async logout(session) {
-    sessions.remove(session);
-  },
-  async authorize(authLogin, authPassword) {
-    const user = await getUser(authLogin);
-
-    if (!user) {
-      return {
-        error: 'Такой пользователь не найден',
-        res: null,
-      };
-    }
-
-    if (authPassword !== user.password) {
-      return {
-        error: 'Неверный пароль',
-        res: null,
-      };
-    }
-
-    return {
-      error: null,
-      res: {
-        id: user.id,
-        login: user.login,
-        roleId: user.role_id,
-        session: sessions.create(user),
-      },
-    };
-  },
-  async register(regLogin, regPassword) {
-    const user = await getUser(regLogin);
-
-    if (user) {
-      return {
-        error: 'Такой логин уже занят',
-        res: null,
-      };
-    }
-
-    const addedUser = await addUser(regLogin, regPassword);
-
-    return {
-      error: null,
-      res: {
-        id: addedUser.id,
-        login: addedUser.login,
-        roleId: addedUser.role_id,
-        session: sessions.create(addedUser),
-      },
-    };
-  },
+  authorize,
+  logout,
+  register,
 };
